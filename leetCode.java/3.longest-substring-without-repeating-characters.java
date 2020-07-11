48558a86fb2d1a0942c272f7f8b5113ee435e192
wbbbbbz <wbbbbbz@gmail.com>
2020-07-11 17:07:36 +0900

    3.longest-substring-without-repeating-characters

--- leetCode.java/3.longest-substring-without-repeating-characters.java
+++ leetCode.java/3.longest-substring-without-repeating-characters.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 import javax.xml.stream.events.Characters;
 
@@ -57,29 +58,60 @@ import javax.xml.stream.events.Characters;
 // @lc code=start
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.isEmpty())
+        if (s == null || s.isEmpty())
             return 0;
-        // 记录每一个字母最后出现的index
-        HashMap<Character, Integer> lastIndex = new HashMap<>();
-        // 现有子字符串的开始index
-        int start = 0;
-        // 最长子字符串的长度
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            Character c = s.charAt(i);
-            if (!lastIndex.containsKey(c)) {
-                lastIndex.put(c, i);
-            } else {
-                start = start > lastIndex.get(c) + 1 ? start : lastIndex.get(c) + 1;
-                lastIndex.put(c, i);
-            }
-            res = res > i - start ? res : i - start;
-            // System.out.println(start + ", " + res);
+        if (s.length() == 1) {
+            return 1;
         }
-        return res + 1;
-        // 987/987 cases passed (4 ms)
-        // Your runtime beats 88.12 % of java submissions
-        // Your memory usage beats 11.1 % of java submissions (39.9 MB)
+        // // 记录每一个字母最后出现的index
+        // HashMap<Character, Integer> lastIndex = new HashMap<>();
+        // // 现有子字符串的开始index
+        // int start = 0;
+        // // 最长子字符串的长度
+        // int res = 0;
+        // for (int i = 0; i < s.length(); i++) {
+        // Character c = s.charAt(i);
+        // if (!lastIndex.containsKey(c)) {
+        // lastIndex.put(c, i);
+        // } else {
+        // start = start > lastIndex.get(c) + 1 ? start : lastIndex.get(c) + 1;
+        // lastIndex.put(c, i);
+        // }
+        // res = res > i - start ? res : i - start;
+        // // System.out.println(start + ", " + res);
+        // }
+        // return res + 1;
+        // // 987/987 cases passed (4 ms)
+        // // Your runtime beats 88.12 % of java submissions
+        // // Your memory usage beats 11.1 % of java submissions (39.9 MB)
+
+        // 滑动窗口
+        // 每次都检查右边界i和[left, i-1]部分是否有重复，没有就继续下一个有边界
+        // 如果有的话，首先更新一下长度，然后更新左边界。
+        // 因为接下来的以右边界的最大连续子字符串一定无法到达旧的left了。
+        // 是否右重复就通过hashSet来记录
+        HashSet<Character> charSet = new HashSet<>();
+        int left = 0;
+        int maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char iChar = s.charAt(i);
+            if (charSet.contains(iChar)) {
+                while (charSet.contains(iChar)) {
+                    charSet.remove(s.charAt(left++));
+                }
+                charSet.add(iChar);
+            } else {
+                charSet.add(iChar);
+                maxLen = Math.max(maxLen, i - left + 1);
+            }
+            // System.out.println(charSet);
+            // System.out.println("left: " + left + ", maxLen: " + maxLen);
+        }
+        return maxLen;
+
+        // 987/987 cases passed (11 ms)
+        // Your runtime beats 34.96 % of java submissions
+        // Your memory usage beats 5.38 % of java submissions (43.6 MB)
     }
 }
 // @lc code=end
