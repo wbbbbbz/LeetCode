8084f757db6237b1e0748a78b2645520d2c899c2
wbbbbbz <wbbbbbz@gmail.com>
2020-07-26 19:56:57 +0900

    46.permutations

--- README.md
+++ README.md
@@ -15,6 +15,7 @@
   - [24.swap-nodes-in-pairs](leetCode.notes/24.swap-nodes-in-pairs.md)
   - [26.remove-duplicates-from-sorted-array](leetCode.notes/26.remove-duplicates-from-sorted-array.md)
   - [27.remove-element](leetCode.notes/27.remove-element.md)
+  - [46.permutations](leetCode.notes/46.permutations.md)
   - [49.group-anagrams](leetCode.notes/49.group-anagrams.md)
   - [61.rotate-list](leetCode.notes/61.rotate-list.md)
   - [75.sort-colors](leetCode.notes/75.sort-colors.md)
---
+++ leetCode.java/46.permutations.java
@@ -0,0 +1,76 @@
+import java.util.*;
+/*
+ * @lc app=leetcode id=46 lang=java
+ *
+ * [46] Permutations
+ *
+ * https://leetcode.com/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (61.20%)
+ * Likes:    3982
+ * Dislikes: 105
+ * Total Accepted:    618.8K
+ * Total Submissions: 978.3K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a collection of distinct integers, return all possible permutations.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * Output:
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [1,3,2],
+ * ⁠ [2,1,3],
+ * ⁠ [2,3,1],
+ * ⁠ [3,1,2],
+ * ⁠ [3,2,1]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+
+    private List<List<Integer>> res = new LinkedList<>();
+
+    private int[] nums;
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        if (nums == null || nums.length == 0){
+            return res;
+        }
+
+        this.nums = nums;
+        
+        for (int i = 0; i < nums.length; i++) {
+            boolean[] visited = new boolean[nums.length];
+            visited[i] = true;
+            genPermutation(visited, nums.length - 1, new LinkedList<Integer>(Arrays.asList(nums[i])));
+        }
+        return res;
+    }
+
+    private void genPermutation(boolean[] visited, int rest, LinkedList<Integer> perm){
+        if (rest == 0){
+            res.add(new LinkedList<Integer>(perm));
+        }
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]){
+                visited[i] = true;
+                LinkedList<Integer> temp = new LinkedList<>(perm);
+                temp.add(this.nums[i]);
+                genPermutation(visited.clone(), rest - 1, temp);
+                visited[i] = false;
+            }
+        }
+        // 2ms
+    }
+}
+// @lc code=end
+
