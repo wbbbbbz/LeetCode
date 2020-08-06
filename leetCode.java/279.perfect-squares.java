1d2a4d4f829629d62f0f7c90164dba3f59bea8fe
wbbbbbz <wbbbbbz@gmail.com>
2020-08-06 22:25:10 +0900

    279.perfect-squares

--- leetCode.java/279.perfect-squares.java
+++ leetCode.java/279.perfect-squares.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 /*
@@ -39,36 +40,69 @@ class Solution {
         if (n <= 0){
             return -1;
         }
-        // 使用Queue进行bfs搜索，使用boolean[]进行记录是否访问过
-        // 类似于图的解法
-        // 其实就是图论的解法。背后的图是从0到n的无权图，结点之间差距完全平方数的话就有一条边
-        boolean[] visited = new boolean[n];
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-        queue.add(n);
-        int steps = 1;
-        while (true){
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int temp = queue.pollFirst();
-                for (int j = 1; ; j++) {
-                    int rest = temp - j * j;
-                    if (rest < 0){
-                        break;
-                    }
-                    if (rest == 0){
-                        return steps;
-                    }
-                    if (!visited[rest]){
-                        queue.addLast(rest);
-                        visited[rest] = true;
-                    }
-                }
+        // DP1
+        // int[] times = new int[n + 1];
+        // Arrays.fill(times, Integer.MAX_VALUE);
+        // for (int i = 1; i * i <= n; i++) {
+        //     times[i * i] = 1;
+        // }
+        // for (int i = 1; i <= n; i++) {
+        //     if (times[i] != Integer.MAX_VALUE){
+        //         for (int j = 1; i + j * j <= n; j++) {
+        //             times[i + j * j] = Math.min(times[i + j * j], times[i] + 1);
+        //         }
+        //     }
+        // }
+        // // System.out.println(Arrays.toString(times));
+        // return times[n];
+
+        // DP2
+        int[] times = new int[n + 1];
+        times[1] = 1;
+        int index = 2;
+        while (index <= n){
+            int temp = Integer.MAX_VALUE;
+            for (int i = 1; index - i * i >= 0; i++) {
+                temp = Math.min(temp, times[index - i * i] + 1);
             }
-            steps++;
+            times[index] = temp;
+            index++;
+            // System.out.println(Arrays.toString(times));
         }
-//         588/588 cases passed (7 ms)
-// Your runtime beats 95.49 % of java submissions
-// Your memory usage beats 26.9 % of java submissions (39.5 MB)
+        return times[n];
+        // 21ms, 40.8MB
+
+        
+//         // 使用Queue进行bfs搜索，使用boolean[]进行记录是否访问过
+//         // 类似于图的解法
+//         // 其实就是图论的解法。背后的图是从0到n的无权图，结点之间差距完全平方数的话就有一条边
+//         boolean[] visited = new boolean[n];
+//         LinkedList<Integer> queue = new LinkedList<Integer>();
+//         queue.add(n);
+//         int steps = 1;
+//         while (true){
+//             int size = queue.size();
+//             for (int i = 0; i < size; i++) {
+//                 int temp = queue.pollFirst();
+//                 for (int j = 1; ; j++) {
+//                     int rest = temp - j * j;
+//                     if (rest < 0){
+//                         break;
+//                     }
+//                     if (rest == 0){
+//                         return steps;
+//                     }
+//                     if (!visited[rest]){
+//                         queue.addLast(rest);
+//                         visited[rest] = true;
+//                     }
+//                 }
+//             }
+//             steps++;
+//         }
+// //         588/588 cases passed (7 ms)
+// // Your runtime beats 95.49 % of java submissions
+// // Your memory usage beats 26.9 % of java submissions (39.5 MB)
     }
 }
 // @lc code=end
